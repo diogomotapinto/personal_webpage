0076f57bec92b4425a6ab7aecec8eb9005f05aeb
diogomotapinto <diogomotapinto@gmail.com>
2022-09-23 00:21:28 +0100

    Text added - bugfix

--- client/components/Interactive/index.tsx
+++ client/components/Interactive/index.tsx
@@ -3,6 +3,7 @@ import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { Physics, usePlane, useSphere } from "@react-three/cannon";
 import { EffectComposer, SSAO, Bloom } from "@react-three/postprocessing";
 import { Text } from "@react-three/drei";
+import { Suspense } from "react";
 
 export default function Interactive() {
   return (
@@ -11,32 +12,35 @@ export default function Interactive() {
       gl={{ stencil: false, antialias: false }}
       camera={{ position: [0, 0, 20], fov: 50, near: 17, far: 40 }}
     >
-      <fog attach="fog" args={["#2c54bb", 25, 35]} />
-      <color attach="background" args={["#ffffff"]} />
-      <ambientLight intensity={1.5} />
-      <directionalLight position={[-10, -10, -5]} intensity={0.5} />
-      <directionalLight
-        castShadow
-        intensity={4}
-        position={[50, 50, 25]}
-        shadow-mapSize={[256, 256]}
-        shadow-camera-left={-10}
-        shadow-camera-right={10}
-        shadow-camera-top={10}
-        shadow-camera-bottom={-10}
-      />
-      <Caption>{`Software Developer\nBased in Porto.`}</Caption>
-      <Physics
-        gravity={[0, -50, 0]}
-        defaultContactMaterial={{ restitution: 0.5 }}
-      >
-        <group position={[0, 0, -10]}>
-          <Mouse />
-          <Borders />
-          <InstancedSpheres />
-        </group>
-      </Physics>
-      {/* <EffectComposer>
+      <Suspense fallback={null}>
+        <fog attach="fog" args={["#2c54bb", 25, 35]} />
+        <color attach="background" args={["#ffffff"]} />
+        <ambientLight intensity={1.5} />
+        <directionalLight position={[-10, -10, -5]} intensity={0.5} />
+        <directionalLight
+          castShadow
+          intensity={4}
+          position={[50, 50, 25]}
+          shadow-mapSize={[256, 256]}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+
+        <Caption>{`Software Developer\nBased in Porto.`}</Caption>
+
+        <Physics
+          gravity={[0, -50, 0]}
+          defaultContactMaterial={{ restitution: 0.5 }}
+        >
+          <group position={[0, 0, -10]}>
+            <Mouse />
+            <Borders />
+            <InstancedSpheres />
+          </group>
+        </Physics>
+        {/* <EffectComposer>
         <SSAO
           radius={0.4}
           intensity={50}
@@ -50,6 +54,7 @@ export default function Interactive() {
           luminanceSmoothing={0.0}
         />
       </EffectComposer> */}
+      </Suspense>
     </Canvas>
   );
 }
@@ -80,7 +85,6 @@ function Caption({ children }) {
     <Text
       position={[0, 0, -5]}
       lineHeight={0.8}
-      font="/Ki-Medium.ttf"
       fontSize={width / 8}
       color="black"
       material-toneMapped={false}
