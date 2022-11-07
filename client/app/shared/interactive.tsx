78e36379036dda99c8ded69c48f118a44cf03b3a
diogomotapinto <diogomotapinto@gmail.com>
2022-11-07 15:41:47 +0000

    Look improvements

--- client/app/shared/interactive.tsx
+++ client/app/shared/interactive.tsx
@@ -8,41 +8,43 @@ import { Suspense } from "react";
 
 export default function Interactive() {
   return (
-    <Canvas
-      shadows
-      gl={{ stencil: false, antialias: false }}
-      camera={{ position: [0, 0, 20], fov: 50, near: 17, far: 40 }}
-    >
-      <Suspense fallback={null}>
-        <fog attach="fog" args={["#2c54bb", 25, 35]} />
-        <color attach="background" args={["#ffffff"]} />
-        <ambientLight intensity={1.5} />
-        <directionalLight position={[-10, -10, -5]} intensity={0.5} />
-        <directionalLight
-          castShadow
-          intensity={4}
-          position={[50, 50, 25]}
-          shadow-mapSize={[256, 256]}
-          shadow-camera-left={-10}
-          shadow-camera-right={10}
-          shadow-camera-top={10}
-          shadow-camera-bottom={-10}
-        />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Canvas
+        shadows
+        gl={{ stencil: false, antialias: false }}
+        camera={{ position: [0, 0, 20], fov: 50, near: 17, far: 40 }}
+      >
+        <Suspense fallback={null}>
+          <fog attach="fog" args={["#2c54bb", 25, 35]} />
+          <color attach="background" args={["#ffffff"]} />
+          <ambientLight intensity={1.5} />
+          <directionalLight position={[-10, -10, -5]} intensity={0.5} />
+          <directionalLight
+            castShadow
+            intensity={4}
+            position={[50, 50, 25]}
+            shadow-mapSize={[256, 256]}
+            shadow-camera-left={-10}
+            shadow-camera-right={10}
+            shadow-camera-top={10}
+            shadow-camera-bottom={-10}
+          />
 
-        <Caption>{`Diogo\nSoftware Developer\nBased in Porto.`}</Caption>
+          <Caption>{`Diogo\nSoftware Developer\nBased in Porto.`}</Caption>
 
-        <Physics
-          gravity={[0, -50, 0]}
-          defaultContactMaterial={{ restitution: 0.5 }}
-        >
-          <group position={[0, 0, -10]}>
-            <Mouse />
-            <Borders />
-            <InstancedSpheres />
-          </group>
-        </Physics>
-      </Suspense>
-    </Canvas>
+          <Physics
+            gravity={[0, -50, 0]}
+            defaultContactMaterial={{ restitution: 0.5 }}
+          >
+            <group position={[0, 0, -10]}>
+              <Mouse />
+              <Borders />
+              <InstancedSpheres />
+            </group>
+          </Physics>
+        </Suspense>
+      </Canvas>
+    </Suspense>
   );
 }
 
